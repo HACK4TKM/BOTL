a9963cd2a3228580fd6735a4a358062c55973cf1
Adith Ramdas <aditramdas@gmail.com>
2022-12-09 20:49:53 +0530

    Third

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import "./Main.css"
+import React from "react";
+import "./Main.css";
+import { Link } from "react-router-dom";
+
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="Main">
+      <h1>Hello</h1>
+      <Link to="/sponsor">
+        <button type="button" className="btn">
+          Sponsor Login
+        </button>
+      </Link>
+      <Link to="/eventman">
+        <button type="button" className="btn">
+          Event Manager Login
+        </button>
+      </Link>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
