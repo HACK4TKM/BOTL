581027fbebe863c5b5c3d9c4e6505031ba03a215
Adith Ramdas <aditramdas@gmail.com>
2022-12-09 22:17:52 +0530

    fourth

--- src/Main.js
+++ src/Main.js
@@ -5,17 +5,18 @@ import { Link } from "react-router-dom";
 function Main() {
   return (
     <div className="Main">
-      <h1>Hello</h1>
-      <Link to="/sponsor">
-        <button type="button" className="btn">
-          Sponsor Login
-        </button>
-      </Link>
-      <Link to="/eventman">
-        <button type="button" className="btn">
-          Event Manager Login
-        </button>
-      </Link>
+      <div className="box">
+        <Link to="/sponsor">
+          <button type="button" className="btn">
+            Sponsor Login
+          </button>
+        </Link>
+        <Link to="/eventman">
+          <button type="button" className="btn">
+            Event Manager Login
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
