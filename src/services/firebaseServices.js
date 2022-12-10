2807c2373dab0eb468f62bb92005913140bcac56
JaVD054 <jeedu124@gmail.com>
2022-12-10 09:52:59 +0530

    doesUser exist function

--- src/services/firebaseServices.js
+++ src/services/firebaseServices.js
@@ -1,5 +1,5 @@
 import {app,db,auth} from '../lib/firebaseConfig';
-import { doc, collection, addDoc,getDoc ,getDocs,setDoc} from 'firebase/firestore';
+import { doc, collection, addDoc,getDoc ,getDocs,setDoc,where, query} from 'firebase/firestore';
 
 export const addSponsor = async (name,email) => {
     const res = await addDoc(collection(db, "sponsors"), {name,email});
@@ -10,31 +10,7 @@ export const addEvent = async (name,description,location,startDate,endDate) => {
     const res = await addDoc(collection(db, "events"), {name,description,location,startDate,endDate});
     console.log(res);
 }
-export async function add(){
 
-    const citiesRef = collection(db, "cities");
-
-    await setDoc(doc(citiesRef, "SF"), {
-        name: "San Francisco", state: "CA", country: "USA",
-        capital: false, population: 860000,
-        regions: ["west_coast", "norcal"] });
-    await setDoc(doc(citiesRef, "LA"), {
-        name: "Los Angeles", state: "CA", country: "USA",
-        capital: false, population: 3900000,
-        regions: ["west_coast", "socal"] });
-    await setDoc(doc(citiesRef, "DC"), {
-        name: "Washington, D.C.", state: null, country: "USA",
-        capital: true, population: 680000,
-        regions: ["east_coast"] });
-    await setDoc(doc(citiesRef, "TOK"), {
-        name: "Tokyo", state: null, country: "Japan",
-        capital: true, population: 9000000,
-        regions: ["kanto", "honshu"] });
-    await setDoc(doc(citiesRef, "BJ"), {
-        name: "Beijing", state: null, country: "China",
-        capital: true, population: 21500000,
-        regions: ["jingjinji", "hebei"] });
-}
 
 export async function get(){
     const docRef = doc(db, "cities", "SF");
@@ -49,7 +25,8 @@ export async function get(){
 }
 
 export async function DoesUserExist(email) {
-    const snapshot = await getDocs(doc(collection(db, "sponsors")));
+    const sponsorRef = collection(db, "sponsors");
+    const snapshot = await query(sponsorRef, where("email", "==", email));
     console.log(snapshot)
 
     if (snapshot.empty) {
